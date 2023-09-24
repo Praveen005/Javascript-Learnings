0a88752fa0d5ea5cd21ba32203dd6847d5311517
Praveen005 <praveen08h@gmail.com>
2023-09-24 18:05:35 +0530

    Fixed New-Quote button functionality and made some style changes

--- Projects/Javascript-Quote-Generator/script.js
+++ Projects/Javascript-Quote-Generator/script.js
@@ -1,4 +1,27 @@
 //Variables
-const blockquoteText= document.getElementsByClassName('blockquote-text');
-const speakerName= document.getElementsByClassName('speaker-name');
+const nextQuote= document.querySelector('.newQuoteBtn');
+const quoteText= document.querySelector('.blockquote-text');
+const speakerName= document.querySelector('.speaker-name');
 const newQuoteBtn= document.querySelector('.newQuoteBtn');
+const tweetBtn= document.querySelector('.tweetBtn');
+const whatsAppShareBtn= document.querySelector('.whatsAppShareBtn');
+const instagramShareBtn= document.querySelector('.instagramShareBtn');
+
+
+nextQuote.onclick = function(){
+
+    const randomIndex= Math.floor(Math.random()*quotes.length);
+    console.log(randomIndex, quotes[randomIndex].author);
+    
+    quoteText.innerText = '"' + quotes[randomIndex].text + '"';
+    
+
+    speakerName.innerText = '-' + quotes[randomIndex].author;
+}
+
+
+
+/*
+The Math.floor() function in JavaScript is used to round a number down to the nearest integer that is less than or equal to the original number. 
+
+*/
